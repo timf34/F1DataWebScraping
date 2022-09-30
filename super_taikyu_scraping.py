c034859845a3d9db9ce9e708acf28c0aa12f5fd2
timf34 <farrelti@tcd.ie>
2022-09-30 13:33:06 -0400

    initial commit

---
+++ super_taikyu_scraping.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+class SuperTaikyuScraping:
+    def __init__(self):
+        self.url: str = "https://www.supertaikyu.live/timings/"
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
