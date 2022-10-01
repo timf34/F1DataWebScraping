65d19e03c180ec646383b46f82ae856d5963a08d
timf34 <farrelti@tcd.ie>
2022-10-01 13:26:37 -0400

    saving html to file

--- super_taikyu_scraping.py
+++ super_taikyu_scraping.py
@@ -8,23 +8,32 @@ import time
 class SuperTaikyuScraping:
     def __init__(self):
         self.url: str = "https://www.supertaikyu.live/timings/"
-        self.html = urlopen(self.url)
-        self.soup = BeautifulSoup(self.html, "html.parser")
+        self.driver = webdriver.Edge()
+        self.html: str = self.get_html_using_selenium()
+        self.soup: BeautifulSoup = BeautifulSoup(self.html, "html.parser")
+
+    def get_html_using_selenium(self) -> str:
+        self.driver.get(self.url)
+        time.sleep(3)
+        html = self.driver.page_source
+        self.driver.close()
+        return html
 
     def save_html(self) -> None:
         """
         This saves the html file to the current directory
         """
-        with open("supertaikyu.html", "wb") as f:
-            f.write(self.html.read())
+        print(self.html)
+        with open("supertaikyu.html", "w", encoding="utf-8") as f:
+            f.write(self.html)
+
+    def print_soup(self) -> None:
+        print(self.soup.prettify())
 
 
 def main():
     web_scraper = SuperTaikyuScraping()
-    # print(web_scraper.soup.prettify())
-    web_scraper.print_request()
-
-
+    web_scraper.save_html()
 
 
 if __name__ == "__main__":
