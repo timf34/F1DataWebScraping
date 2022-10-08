3875549b37adbb782e5db4851e9407d398272c45
timf34 <farrelti@tcd.ie>
2022-10-08 22:34:09 +0100

    added func for initializing car_timing_dict

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import pickle as pkl
 from config import TimingTable
-from typing import List
+from typing import List, Dict
 
 
 def save_object_using_pickle(output_path: str, object: TimingTable) -> None:
@@ -20,3 +20,21 @@ def find_indices_of_string(_list: List, string: str) -> List[int]:
     Note: "\n" occurs at every 21st index -> i.e. [20, 41, 62...,1070]
     """
     return [index for index, i in enumerate(_list) if i == string]
+
+
+def get_initialized_car_timing_dict() -> Dict[str, Dict[str, float]]:
+    """
+    This creates a dictionary of the timing table
+    """
+    car_timing_dict: Dict[str, Dict[str, float]] = {}
+
+    # We have hardcoded the car numbers into a txt file for now
+    with open("data/car_nums.txt", "r") as f:
+        car_nums = f.readlines()
+
+    # Just initalize the sector times to 0 for now
+    for car_num in car_nums:
+        car_timing_dict[car_num.replace("\n", "")] = {"S1": 0., "S2": 0., "S3": 0., "S4": 0.}
+
+    # print(car_timing_dict)
+    return car_timing_dict
