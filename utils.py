3c0d445a7c06608921827939e5c7a1b5840765ab
timf34 <farrelti@tcd.ie>
2022-10-08 21:38:34 +0100

    added func for finding the indices of a given string from a list

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pickle as pkl
 from config import TimingTable
+from typing import List
 
 
 def save_object_using_pickle(output_path: str, object: TimingTable) -> None:
@@ -10,3 +11,12 @@ def save_object_using_pickle(output_path: str, object: TimingTable) -> None:
 def open_object_using_pickle(input_path: str) -> TimingTable:
     with open(input_path, "rb") as f:
         return pkl.load(f)
+
+
+def find_indices_of_string(_list: List, string: str) -> List[int]:
+    """
+    This finds the indices of a string in a list
+
+    Note: "\n" occurs at every 21st index -> i.e. [20, 41, 62...,1070]
+    """
+    return [index for index, i in enumerate(_list) if i == string]
