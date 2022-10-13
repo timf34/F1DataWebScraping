1bda97ccddb229070caa0c1f8830da0e962d9695
timf34 <farrelti@tcd.ie>
2022-10-13 12:26:57 +0100

    practice

--- learning/continuous_streaming.py
+++ learning/continuous_streaming.py
@@ -1,4 +1,5 @@
 import asyncio
+from copy import deepcopy
 import time
 
 from typing import Dict, List
@@ -12,18 +13,33 @@ class ContinuousStreaming:
                                   "S3": {"Brake": [1, 2, 3], "Throttle": [4, 5, 6], "Steering": [7, 8, 9]},
                                   "S4": {"Brake": [1, 2, 3], "Throttle": [4, 5, 6], "Steering": [7, 8, 9]}}
 
+        self.sample_dict_with_nums: Dict[str, Dict] = {"1": deepcopy(self.sample_dict), "2": deepcopy(self.sample_dict)}
+
+        self.sectors: List[str] = ["S1", "S2", "S3", "S4"]
         self.actions: List[str] = ["Brake", "Throttle", "Steering"]
 
     def loop_through_dict_forever(self) -> None:
         """
         This function loops through self.sample_dict on a loop forever. Iterating through S1, S2 ..., S4 and back again
         """
+
         while True:
-            for key in self.sample_dict:
-                # Loop through all the actions at once using unpacking and zip
-                for action, values in zip(self.actions, self.sample_dict[key].values()):
-                    print(action, values, key)
-                    time.sleep(0.1)
+            for sector in self.sectors:
+                for action in self.actions:  # Brake -> Speed, etc.
+                    for num in self.sample_dict_with_nums.keys():
+                        print(num, action, sector, self.sample_dict_with_nums[num][sector][action])
+                        time.sleep(0.1)
+
+        # Lets rewrite the loop above, so that it iterates through the dict at each index for each action at the same
+        # time (i.e. it steps through the indices of all the actions in order)
+        # i.e. 1 Brake S1 1, 2 Brake S2 1, 1 Throttle
+
+
+
+
+
+
+
 
 
 
